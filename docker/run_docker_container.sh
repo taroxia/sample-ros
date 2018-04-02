8f1174e5d3b52a1c431c4d2b4e3e788200e0e254
taro_sakamoto@sensetime.com <taro_sakamoto@sensetime.com>
2018-04-02 11:12:40 +0900

    Add sudo user

--- docker/run_docker_container.sh
+++ docker/run_docker_container.sh
@@ -3,11 +3,6 @@ nvidia-docker run --interactive --tty --rm \
   --user $(id --user):$(id --group) \
   --env DISPLAY=${DISPLAY} \
   --env QT_X11_NO_MITSHM=1 \
-  --workdir /home/${USER} \
-  --volume /home/$USER:/home/$USER \
-  --volume /etc/group:/etc/group:ro \
-  --volume /etc/passwd:/etc/passwd:ro \
-  --volume /etc/shadow:/etc/shadow:ro \
-  --volume /etc/sudoers.d:/etc/sudoers.d:ro \
+  --volume ${PWD}:/workspace \
   --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
   sample-ros:latest /bin/bash
